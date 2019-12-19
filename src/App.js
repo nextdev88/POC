7d3814880596ed80410efe92be3c58e26274ff10
nextdev88 <59033013+nextdev88@users.noreply.github.com>
2019-12-19 14:44:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <code> editing src/App.js</code> and save to reload.
+         <code> editings src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
